a707835e4202d6905731eaa48b05f3cb0fe84eed
Umar Chikobava <40596413+chikobava@users.noreply.github.com>
2023-02-05 14:15:32 +0000

    adds setup.cfg and a few tests

--- labs/02_writing_test_assertions/test_stack.py
+++ labs/02_writing_test_assertions/test_stack.py
@@ -14,16 +14,26 @@ class TestStack(TestCase):
 
     def test_push(self):
         """Test pushing an item into the stack"""
-        raise Exception("not implemented")
+        self.stack.push(42)
+        self.assertEqual(42,self.stack.peek())
 
     def test_pop(self):
         """Test popping an item of off the stack"""
-        raise Exception("not implemented")
+        self.stack.push(42)
+        self.assertEqual(42,self.stack.peek())
+        self.assertEqual(42,self.stack.pop())
+        self.assertEqual(True,self.stack.is_empty())
 
     def test_peek(self):
         """Test peeking at the top the stack"""
-        raise Exception("not implemented")
+        self.stack.push(42)
+        self.stack.push(44)
+        self.assertEqual(44,self.stack.peek())
+        self.assertEqual(44,self.stack.pop())
+        self.assertEqual(42,self.stack.peek())
+        self.assertEqual(42,self.stack.pop())
+        self.assertEqual(True,self.stack.is_empty())
 
     def test_is_empty(self):
         """Test if the stack is empty"""
-        raise Exception("not implemented")
+        self.assertEqual(True,self.stack.is_empty())
